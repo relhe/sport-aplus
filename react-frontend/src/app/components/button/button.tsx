aa8f36d306fc3637fc76d88946ff10bd62c60ad6
Renel Lherisson <renellherisson@gmail.com>
2024-12-23 19:01:23 -0500

    RL | created some components

---
+++ react-frontend/src/app/components/button/button.tsx
@@ -0,0 +1,15 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+    return (
+        <button
+            {...props}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+            {children}
+        </button>
+    );
+};
+export default Button;
---
+++ react-frontend/src/app/components/navbar/navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar: React.FC = () => {
+    return (
+        <div>
+            <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+                <p>navigation</p>
+            </nav>
+        </div>
+    );
+};
+export default Navbar;
